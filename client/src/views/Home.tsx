a55a11f23174926aff97af62b46b4208b7a4b5fe
Vladimir Litvintsev <vl.litvintsev@yandex.ru>
2022-10-16 00:15:11 +0300

    front WIP

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,24 +1,89 @@
-import { FC, useEffect, useState, useRef } from "react";
+import { FC, useEffect, useRef } from "react";
+import { Chart, ChartItem, registerables } from "chart.js";
+
+Chart.register(...registerables);
+let isChartsInited = false;
 
 export const Home: FC = () => {
   const streamImageRef = useRef<HTMLImageElement | null>(null);
 
+  const chart1Ref = useRef<HTMLCanvasElement | null>(null);
+  const chart2Ref = useRef<HTMLCanvasElement | null>(null);
+  const chart3Ref = useRef<HTMLCanvasElement | null>(null);
+  const chart4Ref = useRef<HTMLCanvasElement | null>(null);
+
   useEffect(() => {
-    const socket = new WebSocket("ws://localhost:80/ws");
+    if (!isChartsInited && chart1Ref.current) {
+      isChartsInited = true;
 
-    socket.onmessage = (event) => {
-      const image = event.data;
+      const ctx = chart1Ref.current.getContext("2d") as ChartItem;
+      const socket = new WebSocket("ws://localhost:80/ws");
 
-      if (streamImageRef.current) {
-        streamImageRef.current.src = `data:image/png;base64,${image}`;
-      }
-    };
+      const chart1 = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: Array(100)
+            .fill(null)
+            .map(() => ""),
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [0],
+              backgroundColor: "#ffd736",
+              borderColor: "#ffd736",
+              borderWidth: 1,
+              pointRadius: 2,
+            },
+          ],
+        },
+        options: {
+          animation: {
+            duration: 0,
+          },
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+            x: {
+              max: 100,
+            },
+          },
+        },
+      });
+
+      setInterval(() => {
+        chart1.data.datasets[0].data.push((Math.random() * 50) | 0);
+
+        if (chart1.data.datasets[0].data.length > 100) {
+          chart1.data.datasets[0].data = chart1.data.datasets[0].data.slice(1);
+        }
+
+        chart1.update();
+      }, 100);
+
+      socket.onmessage = (event) => {
+        const image = event.data;
+
+        if (streamImageRef.current) {
+          streamImageRef.current.src = `data:image/png;base64,${image}`;
+        }
+      };
+    }
   }, []);
 
   return (
     <div className="page">
-      <h1>Взлом золота</h1>
-      <img ref={streamImageRef} src="" alt="" className="stream" />
+      <div className="left-items">
+        <canvas className="chart1" ref={chart1Ref}></canvas>
+        <div className="chartsContainer">
+          <canvas className="chart2" ref={chart2Ref}></canvas>
+          <canvas className="chart3" ref={chart3Ref}></canvas>
+        </div>
+        <canvas className="chart4" ref={chart4Ref}></canvas>
+      </div>
+      <div className="right-items">
+        <img ref={streamImageRef} src="" alt="" className="stream" />
+      </div>
     </div>
   );
 };
